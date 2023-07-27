5cf2d39f997575e5fb5d53d6524f241f95bc6828
Matt Wing <mattw@dotcomjungle.com>
2023-07-27 11:57:24 -0700

    Initial commit

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,34 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+import { PrismicNextLink } from "@prismicio/next";
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const heroStyle = {
+    backgroundImage: `url(${slice.primary.background.url})`,
+    backgroundColor: slice.primary.overlaycolor ? slice.primary.overlaycolor : "",
+    backgroundBlendMode: "multiply",
+    backgroundSize: "cover",
+
+  }
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      style={heroStyle}
+    >
+      <h1>{slice.primary.title}</h1>
+      {slice.items.map(item => <PrismicNextLink field={item.buttonlink} >{item.buttonlabel}</PrismicNextLink>)}
+    </section>
+  );
+};
+
+export default Hero;
