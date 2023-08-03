2aec4e0526f09ab8fca0e58557d38d78a3a94e73
Matt Wing <mattw@dotcomjungle.com>
2023-08-03 11:39:15 -0700

    Move template to catch uid

--- src/app/post/[uid]/page.tsx
+++ src/app/post/[uid]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
+import { PrismicRichText } from "@prismicio/react";
 import { components } from "@/slices";
 
 type Params = { uid: string };
@@ -14,7 +15,7 @@ const clientConfig = {
           type: 'post',
           uid: 'post',
           lang: 'en-us',
-          path: '/',    
+          path: '/post/:uid',    
         }
     ]
   }
@@ -25,7 +26,21 @@ export default async function Page({ params }: { params: Params }) {
     .getByUID("post", params.uid)
     .catch(() => notFound());
 
-  return <SliceZone slices={page.data.slices} components={components} />;
+  return (
+  <>
+    <h1>{page.data.title}</h1>
+    <article>
+        <PrismicRichText field={page.data.content}/>
+        <h2>Recommendations</h2>
+        {page.data.recommendation.map(recommendation => {return(
+            <section>
+                <h3>{recommendation.type}</h3>
+                <div><PrismicRichText field={recommendation.explanation}/></div>
+            </section>
+        )})}
+    </article>
+  </>
+  );
 }
 
 export async function generateMetadata({
@@ -33,7 +48,7 @@ export async function generateMetadata({
 }: {
   params: Params;
 }): Promise<Metadata> {
-  const client = createClient();
+  const client = createClient(clientConfig);
   const page = await client.getByUID("post", params.uid);
 
   return {
@@ -43,7 +58,7 @@ export async function generateMetadata({
 }
 
 export async function generateStaticParams() {
-  const client = createClient();
+  const client = createClient(clientConfig);
   const pages = await client.getAllByType("post");
 
   return pages.map((page) => {
